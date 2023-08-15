618a95198abbe135b218fa8cd06043297a8c1c92
Rupesh Khadka <rupesss09@gmail.com>
2023-08-15 10:14:01 -0400

    Adding Person Controller to add person.

---
+++ src/main/java/com/springboot/controller/PersonController.java
@@ -0,0 +1,26 @@
+package com.springboot.controller;
+
+import com.springboot.model.Person;
+import com.springboot.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/person")
+public class PersonController {
+
+    private final PersonService personService;
+
+    @Autowired
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
+
+    @PostMapping
+    public void addPerson(@RequestBody Person person){
+        personService.addPerson(person);
+    }
+}
